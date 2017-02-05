be9fb1b6f6cacd5b24e23cd43ddc4bef2d22419f
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-02-05 20:54:31 +0100

    Added some first components

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+`;
+
+const Title = styled.h1`
+  display: inline-block;
+  margin: 20px 20px;
+  color: #FFF;
+`;
+
+export default () => {
+  return (
+    <Wrapper>
+      <Title>Vouddit</Title>
+    </Wrapper>
+  );
+}
