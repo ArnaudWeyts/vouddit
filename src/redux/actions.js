ff3b2221805ffd9efee93f5eb207ae2da294aa69
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-02-07 18:19:10 +0100

    when you forget the 'www' and spend an entire afternoon trying to figure out what's wrong REEEEE

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,14 +2,12 @@ import 'whatwg-fetch';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
 
-const ROOT_URL = 'https://reddit.com';
+const ROOT_URL = 'https://www.reddit.com';
 
 export function fetchPosts(subreddit) {
   const url = `${ROOT_URL}/r/${subreddit}/hot.json?limit=10`;
 
-  const request = fetch(url, {
-      mode: 'no-cors'
-    })
+  const request = fetch(url)
     .then(response => response.json())
     .then(json => json)
     .catch(ex => {
