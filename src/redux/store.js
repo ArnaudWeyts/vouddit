ec61bec9e842e9e95dfea54cfe8d3a7ed067c4f7
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-02-11 17:56:38 +0100

    adjusted store for multiple reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,9 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
-import {postsReducer} from './reducers';
+import rootReducer from './reducers';
 
 const middlewares = [thunk];
-const store = createStore(postsReducer, composeWithDevTools(applyMiddleware(...middlewares)));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
 
 export default store;
