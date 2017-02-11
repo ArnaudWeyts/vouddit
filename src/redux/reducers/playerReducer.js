ade3d5e746f9483d618cc0c246da2c0bc0a7ea30
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-02-11 21:05:20 +0100

    added actions and reducers for player

--- src/redux/reducers/playerReducer.js
+++ src/redux/reducers/playerReducer.js
@@ -1,7 +1,16 @@
-import {TOGGLE_PLAYER} from '../actions/playerActions';
+import {
+  TOGGLE_PLAYER, TOGGLE_CONTROLS, 
+  SET_VOLUME, SEEKING, 
+  SET_DURATION, UPDATE_PLAYED
+} from '../actions/playerActions';
 
 const INITIAL_STATE = {
-  playing: false
+  showControls: false,
+  playing: false,
+  played: 0,
+  volume: 1,
+  seeking: false,
+  duration: null
 };
 
 export function playerReducer(state = INITIAL_STATE, action) {
@@ -9,7 +18,32 @@ export function playerReducer(state = INITIAL_STATE, action) {
     case TOGGLE_PLAYER:
       return {
         ...state,
-        playing: action.toggle
+        playing: action.playing
+      }
+    case TOGGLE_CONTROLS:
+      return {
+        ...state,
+        showControls: action.controls
+      }
+    case UPDATE_PLAYED:
+     return {
+       ...state,
+       played: action.played
+     }
+    case SET_VOLUME:
+      return {
+        ...state,
+        volume: action.volume
+      }
+    case SEEKING:
+      return {
+        ...state,
+        seeking: action.seek
+      }
+    case SET_DURATION:
+      return {
+        ...state,
+        duration: action.duration
       }
     default:
       return state;
