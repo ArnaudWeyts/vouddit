175c055cf0e309273bf4300fafd13c0cef9fb664
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-02-11 16:03:49 +0100

    added rest of icons

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,9 +1,13 @@
 import reddit from './reddit.svg';
+import play_arrow from './play_arrow.svg';
+import pause from './pause.svg';
 import chevron_left from './chevron_left.svg';
 import chevron_right from './chevron_right.svg';
 
 export default {
   reddit,
+  play_arrow,
+  pause,
   chevron_left,
   chevron_right
 }
