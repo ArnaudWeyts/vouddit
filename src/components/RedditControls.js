9d160be9997be4228e256a5586cb781d9d28f430
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-02-13 21:05:10 +0100

    fixed width to prevent overflow

--- src/components/RedditControls.js
+++ src/components/RedditControls.js
@@ -12,7 +12,7 @@ const Wrapper = styled.div`
 
 const Current = styled.div`
   margin: 0 20px;
-  flex-grow: 1;
+  width: 75%;
 `;
 
 const TitleGroup = styled.div`
@@ -24,6 +24,7 @@ const TitleGroup = styled.div`
 const Title = styled.h2`
   white-space: nowrap;
   font-weight: 200;
+  width: 80%;
   margin: 0 auto auto 0;
   max-height: 40px;
   overflow: hidden;
@@ -64,8 +65,10 @@ const Ups = styled.span`
 `;
 
 const Next = styled.div`
+  margin-left: auto;
   display: flex;
   align-items: center;
+  width: 30%;
 `;
 
 const NextVideo = styled.div`
@@ -74,11 +77,8 @@ const NextVideo = styled.div`
   background-size: cover;
   background-position: center;
   height: 100px;
-  width: 200px;
+  width: 100%;
   cursor: pointer;
-  border-radius: 2px;
-  border: none;
-  box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
 `;
 
 const RedditControls = (props) => {
