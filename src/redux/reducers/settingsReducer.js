0bda39c2072f8611a6e88797808f0f1f36182cbf
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-02-22 10:54:40 +0100

    added redux for toggling the window

--- src/redux/reducers/settingsReducer.js
+++ src/redux/reducers/settingsReducer.js
@@ -1,13 +1,22 @@
-import {TOGGLE_USE_DEFAULT} from '../actions/settingsActions';
+import {TOGGLE_SETTINGS, TOGGLE_USE_DEFAULT} from '../actions/settingsActions';
 
 const INITIAL_STATE = {
+  showSettings: false,
   useDefaultPlayer: true
 }
 
 export default function settingsReducer(state = INITIAL_STATE, action) {
   switch(action.type) {
+    case TOGGLE_SETTINGS:
+      return {
+        ...state,
+        showSettings: !state.showSettings
+      }
     case TOGGLE_USE_DEFAULT:
-      return !state.useDefaultPlayer;
+      return {
+        ...state,
+        useDefaultPlayer: !state.useDefaultPlayer
+      }
     default:
       return state;
   }
