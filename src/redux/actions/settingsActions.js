67279681a10f461281eaf134ae263d60f3e27212
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-02-22 10:54:40 +0100

    added redux for toggling the window

--- src/redux/actions/settingsActions.js
+++ src/redux/actions/settingsActions.js
@@ -1,7 +1,14 @@
+export const TOGGLE_SETTINGS = 'TOGGLE_SETTINGS';
 export const TOGGLE_USE_DEFAULT = 'TOGGLE_USE_DEFAULT';
+
+export function toggleSettings() {
+  return {
+    type: TOGGLE_SETTINGS
+  }
+}
 
 export function toggleUseDefault() {
   return {
-    type: TOGGLE_USE_DEFAULT,
+    type: TOGGLE_USE_DEFAULT
   }
 }
