95728656030aaa0479180e0ed6743acf8998ac06
Haroen Viaene <hello@haroen.me>
2017-04-16 01:20:24 +0200

    chore(deps): update dependencies
    not devdependencies because fuck updating webpack 1 to 2

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import ReactPlayer from 'react-player';
 import {connect} from 'react-redux';
 
