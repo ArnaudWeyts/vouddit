377d6493c7d7eed6650b9a00ecd60f64bb6d96b6
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-04-21 20:22:19 +0200

    finished styling, added icon for dropdown

--- src/components/shared/icons/index.js
+++ src/components/shared/icons/index.js
@@ -3,6 +3,7 @@ import play_arrow from './play_arrow.svg';
 import pause from './pause.svg';
 import chevron_left from './chevron_left.svg';
 import chevron_right from './chevron_right.svg';
+import arrow_down from './arrow_down.svg';
 import thumb_up from './thumb_up.svg';
 import person from './person.svg';
 import settings from './settings.svg';
@@ -15,9 +16,10 @@ export default {
   pause,
   chevron_left,
   chevron_right,
+  arrow_down,
   thumb_up,
   person,
   settings,
   menu,
   search
-}
+};
