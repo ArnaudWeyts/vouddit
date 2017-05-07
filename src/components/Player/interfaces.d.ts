c3d2819f541628c3748df1a7234ae0e4d9c06ffc
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-05-07 19:03:47 +0200

    move dispatch, make reusuable for state declaration

--- src/components/Player/interfaces.d.ts
+++ src/components/Player/interfaces.d.ts
@@ -2,12 +2,11 @@ interface IPlayer {
   played: number
   seeking: boolean
   playing: boolean
-  timer: number
   volume: number
   showControls: boolean
-  duration: number
   ended: boolean
-  dispatch: IDispatch<any>
+  duration?: number
+  timer?: number
 }
 
 interface IPlayerProps {
@@ -17,4 +16,5 @@ interface IPlayerProps {
   useDefaultPlayer?: boolean
   isFirst: boolean
   getPrevNextPost: (direction: boolean) => void
+  dispatch: IDispatch<any>
 }
