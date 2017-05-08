12f563d8735ecd1f5c3f86d5bd12ca878e00e0bb
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-05-08 13:14:54 +0200

    post redux typescript

--- src/redux/reducers/interfaces.d.ts
+++ src/redux/reducers/interfaces.d.ts
@@ -1,9 +1,19 @@
-interface IPlayerAction {
-  type: string,
-  play: boolean,
-  played: number,
-  volume: number,
-  seeking: boolean,
-  duration: number,
+interface IAction {
+  type: string
+}
+
+interface IPlayerAction extends IAction {
+  play: boolean
+  played: number
+  volume: number
+  seeking: boolean
+  duration: number
   timer: number
+}
+
+interface IPostsAction extends IAction {
+  posts: { children: Array<IPost>, after: string }
+  update: boolean
+  nextPostId: number
+  subreddit: string
 }
