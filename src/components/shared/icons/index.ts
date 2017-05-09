b625152a5ec54bc82918387500e6fc795d9d6825
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-05-09 11:32:05 +0200

    icon declaration to ts, use require for undeclared imports

---
+++ src/components/shared/icons/index.ts
@@ -0,0 +1,25 @@
+const reddit = require('./reddit.svg');
+const playArrow = require('./play_arrow.svg');
+const pause = require('./pause.svg');
+const chevronLeft = require('./chevron_left.svg');
+const chevronRight = require('./chevron_right.svg');
+const arrowDown = require('./arrow_down.svg');
+const thumbUp = require('./thumb_up.svg');
+const person = require('./person.svg');
+const settings = require('./settings.svg');
+const menu = require('./menu.svg');
+const search = require('./search.svg');
+
+export default {
+  reddit,
+  playArrow,
+  pause,
+  chevronLeft,
+  chevronRight,
+  arrowDown,
+  thumbUp,
+  person,
+  settings,
+  menu,
+  search
+};
