ba162db98748c5a0680941d3df0158fc15e9440f
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-05-19 12:05:25 +0200

    styles yet again

--- src/components/SidePanels/PanelStyles.ts
+++ src/components/SidePanels/PanelStyles.ts
@@ -78,7 +78,12 @@ export const Button = styled.button`
   }
 `;
 
-export const Icon = styled.img`
+export const IconL = styled.img`
   cursor: pointer;
   float: left;
+`;
+
+export const IconR = styled.img`
+  cursor: pointer;
+  float: right;
 `;
