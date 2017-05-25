9ea76bb265b8cf14195696baf94236abcec3ff06
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-05-25 11:57:38 +0200

    material-ui iconmenu

--- src/components/SidePanels/Playlists/PlaylistsList.tsx
+++ src/components/SidePanels/Playlists/PlaylistsList.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import {
-  IconL, IconR
+  IconL
 } from '../PanelStyles';
 
 import {
@@ -10,16 +10,28 @@ import {
 
 import icons from '../../shared/icons';
 
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+
 const PlaylistsList: React.StatelessComponent<{ playlists: Array<IPlaylist> }> = ({ playlists }) => {
   if (playlists.length === 0) { return null!; }
   return (
     <Card>
       {playlists.map(playlist => {
         return (
-          <CardItem>
+          <CardItem key={playlist.name}>
             <IconL src={icons.playArrow} />
             {playlist.name}
-            <IconR src={icons.arrowDown} />
+            <IconMenu
+              iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+              anchorOrigin={{ horizontal: 'left', vertical: 'top' }}
+              targetOrigin={{ horizontal: 'left', vertical: 'top' }}
+            >
+              <MenuItem primaryText="Edit" />
+              <MenuItem primaryText="Delete" />
+            </IconMenu>
           </CardItem>
         );
       })}
