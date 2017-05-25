40d081216ff92acde278b2e9e42fddfde8d758a2
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-05-25 18:31:50 +0200

    revamp add playlist to proper material-ui

--- src/components/SidePanels/Playlists/AddPlaylist.tsx
+++ src/components/SidePanels/Playlists/AddPlaylist.tsx
@@ -3,16 +3,11 @@ import styled from 'styled-components';
 
 import Search from '../../shared/Search';
 
-import {
-  Button, IconR
-} from '../PanelStyles';
-
-import {
-  List, ListItem, Card,
-  Input, CardItem
-} from '../../shared/MainStyles';
-
-import icons from '../../shared/icons';
+import { Card, CardText, CardActions } from 'material-ui/Card';
+import { List, ListItem } from 'material-ui/List';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import DeleteIcon from 'material-ui/svg-icons/action/delete';
 
 const PlaylistWrapper = styled.div`
   margin-top: 10px;
@@ -22,20 +17,19 @@ const AddPlaylist: React.StatelessComponent<IAddPlaylistsProps> = props => {
   const renderList = () => {
     if (props.playlist.subs.length === 0) {
       return (
-        <div>No subs selected</div>
+        <CardText>No subs selected</CardText>
       );
     }
     return (
       <List>
         {props.playlist.subs.map((sub: string) => {
           return (
-            <ListItem key={sub} style={{ color: '#2196F3' }}>
-              {sub}
-              <IconR
-                src={icons.deleteBin}
-                onClick={() => props.updateSub(sub, true)}
-              />
-            </ListItem>
+            <ListItem
+              primaryText={sub}
+              key={sub}
+              rightIcon={<DeleteIcon />}
+              onClick={() => props.updateSub(sub, true)}
+            />
           );
         })}
       </List>
@@ -46,16 +40,15 @@ const AddPlaylist: React.StatelessComponent<IAddPlaylistsProps> = props => {
     <div>
       <PlaylistWrapper>
         <Card>
-          <CardItem>
-            <Input
-              placeholder={props.playlist.name}
+          <CardText>
+            <TextField
+              hintText={props.playlist.name}
               onChange={({ target }) => {
                 const value = (target as HTMLInputElement).value;
                 props.updateName(value);
               }}
+              fullWidth={true}
             />
-          </CardItem>
-          <CardItem>
             <Search
               suggestions={props.searchSubs}
               placeholder="Search for a sub..."
@@ -63,23 +56,20 @@ const AddPlaylist: React.StatelessComponent<IAddPlaylistsProps> = props => {
               onSelected={props.updateSub}
               onClear={props.clearSearchSubs}
             />
-          </CardItem>
-          <CardItem>{renderList()}</CardItem>
-          <CardItem>
-            <Button
-              onClick={() => props.createPlaylist()}
-              style={{ width: '57%', marginRight: '3%' }}
+          </CardText>
+          {renderList()}
+          <CardActions>
+            <RaisedButton
+              primary={true}
+              label="Create"
               disabled={props.playlist.subs.length === 0 ? true : false}
-            >
-              <span>Create</span>
-            </Button>
-            <Button
+              onClick={() => props.createPlaylist()}
+            />
+            <RaisedButton
+              label="Cancel"
               onClick={() => props.toggleAddPlaylist()}
-              style={{ width: '37%', marginLeft: '3%' }}
-            >
-              <span>Cancel</span>
-            </Button>
-          </CardItem>
+            />
+          </CardActions>
         </Card>
       </PlaylistWrapper>
     </div>
