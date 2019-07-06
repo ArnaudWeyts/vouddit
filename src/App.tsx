9fd7d3900714954f2ab193d31efd50df84d30178
Arnaud Weyts <arnaud.weyts@gmail.com>
2019-07-06 23:42:58 +0200

    :lipstick: set up basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { Layout } from "antd";
+import styled from "styled-components";
+
+const { Header, Sider, Content } = Layout;
+
+const FullHeightLayout = styled(Layout)`
+  height: 100%;
+`;
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">Test</header>
-    </div>
+    <FullHeightLayout>
+      <Sider>Sider</Sider>
+      <Layout>
+        <Header>Header</Header>
+        <Content>Content</Content>
+      </Layout>
+    </FullHeightLayout>
   );
 };
 
